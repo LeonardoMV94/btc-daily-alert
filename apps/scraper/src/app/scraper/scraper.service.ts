fe2174a67be81e324378560a14e3f1dceddafa15
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2025-07-14 22:45:57 -0400

    add: comment

--- apps/scraper/src/app/scraper/scraper.service.ts
+++ apps/scraper/src/app/scraper/scraper.service.ts
@@ -1,26 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { CreateScraperDto } from './dto/create-scraper.dto';
-import { UpdateScraperDto } from './dto/update-scraper.dto';
 
 @Injectable()
 export class ScraperService {
-  create(createScraperDto: CreateScraperDto) {
+  scraper(createScraperDto: CreateScraperDto) {
+    // https://alternative.me/crypto/fear-and-greed-index/
     return 'This action adds a new scraper';
-  }
-
-  findAll() {
-    return `This action returns all scraper`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} scraper`;
-  }
-
-  update(id: number, updateScraperDto: UpdateScraperDto) {
-    return `This action updates a #${id} scraper`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} scraper`;
   }
 }
